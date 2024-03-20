455e990a1c546c4a3ea10a37bde4a37ebad18818
Biloshytskyi <obiloshytskyi@luxoft.com>
2024-03-20 10:15:54 +0100

    Only bbox evaluation in COCO Evaluator. Dont evauate segm, since there are no area in annotations for hitl data

--- tfboard_validation/trainer.py
+++ tfboard_validation/trainer.py
@@ -26,7 +26,12 @@ class Trainer(DefaultTrainer):
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+        return COCOEvaluator(
+            dataset_name=dataset_name,
+            tasks=["bbox"],
+            distributed=True,
+            output_dir=output_folder,
+        )
 
     def build_writers(self):
         """
